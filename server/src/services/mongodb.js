0fe7b63604644b20b91b89e2e232f754e13f54ee
luihin903 <luihin903@gmail.com>
2024-03-13 10:37:12 -0700

    Add update() in mongodb.js

--- server/src/services/mongodb.js
+++ server/src/services/mongodb.js
@@ -9,13 +9,30 @@ const uri = `mongodb+srv://${username}:${password}@ace-detailing.sc1kbvv.mongodb
 async function initConnection() {
     try {
         logger.info("Connecting to MongoDB cluster.");
-        await mongoose.connect(uri);
+        let client = await mongoose.connect(uri);
+        return client;
     }
     catch (err) {
         logger.error(err);
     }
 };
 
+async function update(document, object) {
+    let client = initConnection();
+    let db = client.db("cmpt372");
+    const docs = db.collection(document);
+    const res = await docs.updateOne(object);
+
+    const cursor = docs.find();
+    for await (const d of cursor) {
+        console.log(d);
+    }
+
+    client.close();
+    process.exit();
+}
+
 module.exports = {
-    initConnection
+    initConnection,
+    update
 };
