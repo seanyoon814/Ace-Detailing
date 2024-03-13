de54580ce16ea9784ea809c52b5711ea07c53d70
trijaychan <trijaychan@gmail.com>
2024-03-12 23:49:12 -0700

    Move files and folders to src

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require("./userRoutes");
+
+const userRoutes = require("./routes/userRoutes");
+
 require('dotenv').config(); // Env variables
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(express.json());
 app.use(cors());
+
+// routes
 app.use(userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
-
