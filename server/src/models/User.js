27e3351bd3c6711aa8ead4176a325568ad975a5e
luihin903 <luihin903@gmail.com>
2024-03-13 00:05:06 -0700

    fill in User.js and add post("/user/register") in userRoutes.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,3 +1,17 @@
 module.exports = class User {
+    id;
+    email;
+    password;
+    admin;
 
+    constructor(id, email, password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.admin = false;
+    }
+
+    save() {
+        
+    }
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
+const User = require("./models/User");
+
+router.use(bodyParser.urlencoded({extended : true}));
 
 router.get("/user/register", (req, res) => {
+    // TODO: redierect to user register page
     res.send("register");
+})
+
+router.post("/user/register", (req, res) => {
+    // TODO: count user id
+    // TODO: confirm email address
+    var id = generateUserId();
+    var user = new User(id, req.body.email, req.body.password);
+    user.save();
 })
 
 router.get("/user/login", (req, res) => {
     res.send("login");
 })
+
+function generateUserId() {
+    // TODO
+}
 
 module.exports = router;
