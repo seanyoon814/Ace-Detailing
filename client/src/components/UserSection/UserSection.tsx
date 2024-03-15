07d589841e52312a6ba8d1fcb679c02ad122b88a
luihin903 <luihin903@gmail.com>
2024-03-15 00:28:14 -0700

    Change Styles to private

--- client/src/components/UserSection/UserSection.tsx
+++ client/src/components/UserSection/UserSection.tsx
@@ -1,15 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./UserSection.css";
 import { Helmet } from "react-helmet";
 
 function UserSection() {
     return (
-        <div>
+        <div className = "UserSection">
             <Helmet>
                 <title>User Portal</title>
             </Helmet>
-            <a href = "#">Login</a>
+            <a href = "#" className = "UserSection">Login</a>
             <br />
-            <a href = "/user/register">Register</a>
+            <a href = "/user/register" className = "UserSection">Register</a>
             <span id = "hidden"></span>
             <span id = "main-navbar"></span>
             <span id = "header-sidebar-toggle"></span>
