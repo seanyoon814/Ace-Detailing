8ddc08864e161bd3daa1af27fc8da79df1adad4c
trijaychan <trijaychan@gmail.com>
2024-03-18 17:42:32 -0700

    Fix timeout function to not run in non-main pages

--- client/public/script.js
+++ client/public/script.js
@@ -1,75 +1,79 @@
-if ($('.carousel')) {
-    setTimeout(() =>{
-        const hidden = document.getElementById('hidden');
-        const targetElement = document.getElementById('main-navbar');
-        const hiddenElement = document.getElementById('header-sidebar-toggle');
-        const logo = document.getElementById('logo');
-        function handleIntersection(entries, observer) {
-            entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                hiddenElement.style.opacity = 0;
-                logo.style.opacity = 0;
-                logo.style.display ='none';
-                targetElement.style.position = 'fixed';
-                hiddenElement.style.display = 'none';
-            }
-            else
-            {
-                targetElement.style.position = 'absolute';
-                hiddenElement.style.display = 'block';
-                logo.style.display = 'block';
-                setTimeout(() => {
-                    hiddenElement.style.opacity = 1;
-                    logo.style.opacity = 1;
-                }, 50);
-            }
-            });
-        }
+setTimeout(() =>{
+    const hidden = document.getElementById('hidden');
+    const targetElement = document.getElementById('main-navbar');
+    const hiddenElement = document.getElementById('header-sidebar-toggle');
+    const logo = document.getElementById('logo');
 
-        const observerReveal = new IntersectionObserver(handleIntersection);
-        observerReveal.observe(hidden);
+    // error was being thrown at `observerReveal.observe(hidden)`
+    if (!hiddenElement) {
+        return;
+    }
 
-        function animateWhenVisible(entries, observer) {
+    function handleIntersection(entries, observer) {
         entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('reveal-text');
-                observer.unobserve(entry.target);
-            }
-        });
+        if (!entry.isIntersecting) {
+            hiddenElement.style.opacity = 0;
+            logo.style.opacity = 0;
+            logo.style.display ='none';
+            targetElement.style.position = 'fixed';
+            hiddenElement.style.display = 'none';
         }
-        const observerBlock = new IntersectionObserver(animateWhenVisible, { threshold: 0.5 });
-        
-        // Observe each element
-        document.querySelectorAll('.quote').forEach(el => {
-        observerBlock.observe(el);
-        });
-        function animateLeftFadeWhenVisible(entries, observer) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('fade-in-left');
-                observer.unobserve(entry.target);
-            }
-        });
+        else
+        {
+            targetElement.style.position = 'absolute';
+            hiddenElement.style.display = 'block';
+            logo.style.display = 'block';
+            setTimeout(() => {
+                hiddenElement.style.opacity = 1;
+                logo.style.opacity = 1;
+            }, 50);
         }
-        function animateRightFadeWhenVisible(entries, observer) {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('fade-in-right');
-                observer.unobserve(entry.target);
-            }
         });
+    }
+
+    const observerReveal = new IntersectionObserver(handleIntersection);
+    observerReveal.observe(hidden);
+
+    function animateWhenVisible(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('reveal-text');
+            observer.unobserve(entry.target);
         }
-        const observerLeftFade = new IntersectionObserver(animateLeftFadeWhenVisible, { threshold: 0.5 });
-        const observerRightFade = new IntersectionObserver(animateRightFadeWhenVisible, { threshold: 0.5 });
-        document.querySelectorAll('.fade-left').forEach(el => {
-        observerLeftFade.observe(el);
-        });
-        document.querySelectorAll('.fade-right').forEach(el => {
-        observerRightFade.observe(el);
-        });
+    });
+    }
+    const observerBlock = new IntersectionObserver(animateWhenVisible, { threshold: 0.5 });
+    
+    // Observe each element
+    document.querySelectorAll('.quote').forEach(el => {
+    observerBlock.observe(el);
+    });
+    function animateLeftFadeWhenVisible(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in-left');
+            observer.unobserve(entry.target);
+        }
+    });
+    }
+    function animateRightFadeWhenVisible(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in-right');
+            observer.unobserve(entry.target);
+        }
+    });
+    }
+    const observerLeftFade = new IntersectionObserver(animateLeftFadeWhenVisible, { threshold: 0.5 });
+    const observerRightFade = new IntersectionObserver(animateRightFadeWhenVisible, { threshold: 0.5 });
+    document.querySelectorAll('.fade-left').forEach(el => {
+    observerLeftFade.observe(el);
+    });
+    document.querySelectorAll('.fade-right').forEach(el => {
+    observerRightFade.observe(el);
+    });
 
 
-    }, 500);
+}, 500);
 
-    $('.carousel').carousel();
-}
+$('.carousel').carousel();
