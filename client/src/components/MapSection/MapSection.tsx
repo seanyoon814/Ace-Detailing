62e0488cd2f23ac6ce758fab672bef7062748aa6
Sean Yoon <seanyoon@d142-058-223-057.wireless.sfu.ca>
2024-03-19 19:42:32 -0700

    added world map

--- client/src/components/MapSection/MapSection.tsx
+++ client/src/components/MapSection/MapSection.tsx
@@ -9,16 +9,24 @@ function MapSection() {
     <>
       <Header />
       <div id='hidden'></div>
-      <div className='container-fluid'>
+      <div className='container'>
         <div className='row'>
           <div className='col-12 text-center' style={{ marginTop: '30vh' }}>
             <p className='paragraph-noanim lato-light'>COME VISIT US AT</p>
             <h1 className='header garamond' style={{ fontSize: '7em' }}>ACE DETAILING</h1>
           </div>
         </div>
-        <div className='row justify-content-center align-items-center'>
+        <div className='row justify-content-center align-items-center mt-5'>
+          <div className='col-4'>
+            <h1 className='header lato-light'>ACE DETAILING</h1>
+            <ul>
+              <li>
+                
+              </li>
+            </ul>
+          </div>
           <div className='col-8 mb-5'>
-            <MapContainer center={[51.505, -0.09]} zoom={13} style={{height:'10vh'}}>
+            <MapContainer center={[49.1855, -122.845]} zoom={15} style={{height:'60vh'}}>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
---
+++ client/src/components/NL272Section/NL272.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function NL272(){
+    return(
+        <div className='row justify-content-center pt-5'>
+            <div className='col-4 text-center'>
+                <p className='paragraph lato-light' style={{opacity:1}}>NASIOL CANADA WARRANTY : NL272</p>
+                <table>
+                        <tr>
+                            <td><b>5 YEARS</b> <br/>without annual top coat</td>
+                            <td><b>10 YEARS</b> <br/>without annual top coat</td>
+                            <td><b>Declared on Carfax</b></td>
+                        </tr>
+                </table>
+            </div>
+            <div className='col-4 text-center'>
+                <p className='paragraph lato-light' style={{opacity:1}}>NASIOL CANADA WARRANTY : ZR53</p>
+                <table>
+                    <tr>
+                        <td><b>5 YEARS</b> <br/>without annual top coat</td>
+                        <td><b>10 YEARS</b> <br/>without annual top coat</td>
+                        <td><b>Declared on Carfax</b></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default NL272;
