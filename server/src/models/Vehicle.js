dba8f4e4bcc9ec521ae1c548a55485d3a0487e41
trijaychan <trijaychan@gmail.com>
2024-04-02 15:37:21 -0700

    Align vehicle schema with client display

--- server/src/models/Vehicle.js
+++ server/src/models/Vehicle.js
@@ -15,29 +15,10 @@ const VehicleSchema = new Schema(
             type: String,
             required: true
         },
-        vinNumber: {
+        vehicle: {
             type: String,
             required: true,
-            validate: /^[A-Z0-9]{17}$/
-        },
-        make: {
-            type: String,
-            required: true,
-            maxLength: 50
-        },
-        model: {
-            type: String,
-            required: true,
-            maxLength: 50
-        },
-        series: {
-            type: String,
-            required: false,
-            maxLength: 50
-        },
-        year: {
-            type: Number,
-            required: true
+            maxLength: 150
         },
         notes: {
             type: String,
